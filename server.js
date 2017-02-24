3bd5dc10b2a2903d38481cdd6c53ad0dd55c2fa3
David Arteaga <david17art@gmail.com>
2017-02-24 00:45:06 -0400

    Modificacion de el server

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + "public/"))
+
+app.get("*",function(req,res){
+	res.sendfile("public/index.html")
+})
+
 app.listen('3000',function(){
 	console.log("Server instalado")
 })
