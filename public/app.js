de5cab78c36ec923c82969449cd286598e3c9f5f
RealGestion <redes@realgestion.com.ve>
2017-02-28 15:18:55 -0400

    Cambio estable al app.js

--- public/app.js
+++ public/app.js
@@ -1,28 +1,62 @@
-angular.module("Rg-App", ["ngRoute"])
-	.config(function($routeProvider){
-		//Rutas pre-definidas
+/*
+	Rutas de la app
+	(/) La pagina principal
+	(/propiedades) Todas las propiedades
+	(/proyectos) Todas las pre-ventas
+	(/contacto) Pagina de contacto.
+
+	ACOTACIONES
+	en el index.html hay que poner la etiqueta base <base href="/">
+	para que las url funciones bien.
+
+
+	Este es el CLiente para consumir la api de RealGestion.
+	A continuacion rutas de la api:
+	************TIPOS GETS****************************
+	//realgestion.com.ve/api/Aparence/{company} Trae los datos para el template de una compañia, toda la informacion de contacto y el seo 
+	//realgestion.com.ve/api/Propiedades/{company} Trae todas las propiedades de esa compañia
+	//realgestion.com.ve/api/Propiedades/{company}/{id} Trae los datos de una propiedad
+	//realgestion.com.ve/api/Profile/{id} Trae el perfil de un corredor inmobiliario
+	//realgestion.com.ve/api/Photo/{company}/{id} Trae las fotos de un inmueble
+	***********TIPOS POST******************************
+	//realgestion.com.ve/api/Contact/{company} envia los datos de el formulario de contacto
+
+
+
+*/
+
+angular.module('Rg-App',['ngRoute'])
+	//RUTAS
+	.config(function($routeProvider, $locationProvider){
 		$routeProvider
-			.when("/",{
-				templateUrl: "templates/home.html",
-				controller: "HomeController"
-			})
-			.when("/propiedades", {
-				templateUrl: "templates/propiedades.html",
-				controller: "PropiedadesController"
-			})
-			.when("/propiedad/:id" , {
-				templateUrl: "templates/propiedad.html",
-				controller: "PropiedadController"
-			})
-			.when("/nosotros", {
-				templateUrl: "templates/nosotros.html",
-				controller: "AboutController"
-			})
-			.when("/contacto", {
-				 templateUrl: "templates/contacto.html",
-				 controller: "ContactController"
-			})
-			.otherwise({
-				templateUrl: "templates/404.html"
-			})
+		.when("/",{
+			templateUrl : "views/home.html",
+			controller: "HomeController"
+		})
+		.when("/propiedades",{
+			templateUrl: "views/properties.html",
+			controller: "PropertiesController"
+		})
+		.when("/proyectos",{
+			templateUrl: "views/properties.html",
+			controller: "ProyectsController"
+		})
+		.when("/Propiedad/:id",{
+			templateUrl: "views/property.html",
+			controller: "PropertyController"
+
+		})
+		.when("/contacto",{
+			templateUrl: "views/contacts.html",
+			controller: "ContactController"
+		})
+		.otherwise({
+        	template : "<h1>404 error</h1>"
+    	});
+    	$locationProvider.html5Mode(true);
 	})
+	//Variables Globales
+	.run(['$rootScope',function($rootScope){
+		$rootScope.url = "//realgestion.com.ve"
+		$rootScope.year = "2017"
+	}])
